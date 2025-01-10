9d2c52b88a7c98b0b695ab9cbc0cc86e0b7baa12
Armend Berisha <Armend.Berisha96@outlook.com>
2025-01-10 10:18:53 +0100

    Add ToDoList class with basic CRUD operations
    Added a new file `ToDoList.cs` implementing the `ToDoList` class within the `TODOproject` namespace. The class includes a private field `_toDoList` to store to-do items as a list of strings. Added methods `Add` to add items, `Remove` to delete items by index with bounds checking, and `Print` to display all items. Included necessary `using` directives for common namespaces.

---
+++ ToDoList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TODOproject
+{
+    public class ToDoList
+    {
+        private List<string> _toDoList = new();
+
+        
+
+        public void Add(string item)
+        {
+            _toDoList.Add(item);
+        }
+
+        public void Remove(int index)
+        {
+            if (index >= 0 && index < _toDoList.Count)
+            {
+                _toDoList.RemoveAt(index);
+            }
+        }
+
+        public void Print()
+        {
+            foreach (var item in _toDoList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
